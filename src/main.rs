38ecf1d5e672e9fb471f3f9b35b92539654ce4e2
EliasSchaffer <eliasschaffer13@gmail.com>
2026-02-26 14:58:03 +0100

    Add basic network interface selection feature
    Includes `pcap` for device listing and `inquire` for interactive user prompts to select a network interface.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::Write;
+use std::io::stdout;
+use std::process::exit;
+use pcap::Device;
+use inquire::Select;
+
 fn main() {
-    println!("Hello, world!");
+    let selected_device:Device;
+    let devices = Device::list().unwrap();
+    let  device_names = devices
+        .iter()
+        .map(|device| device.desc.clone().unwrap_or(device.name.clone()))
+        .collect::<Vec<String>>();
+    let ans = Select::new("Choose a network port:", device_names.clone()).prompt();
+
+    match ans {
+        Ok(choice) => {
+            let index = device_names.iter().position(|f| *f == choice);
+
+            match index {
+                Some(i) => selected_device = devices[i].clone(),
+                None => {
+                    println!("Error getting Device from List");
+                    exit(1)
+                }
+            }
+        },
+        Err(_) => println!("Selection canceled"),
+    }
+
+
+
+
 }
